973678b8952238d4bab6f270e8a6f260f02b93cb
Evanflow89 <ivanbaragone@gmail.com>
2022-05-30 16:46:19 +0200

    commit

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const app = new Vue({
     el: '#app',
     data: {
-        email: "",
+        email: [],
     },
     created() {
         for (let i = 0; i < 10; i++) {
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
                 .then((res) => {
-                    this.email = res.data.response;
+                    this.email.push(res.data.response);
                 });
         }
     },
