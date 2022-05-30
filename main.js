7daa10481406c4ee6e4c04e83b199fc2e3dcf029
Evanflow89 <ivanbaragone@gmail.com>
2022-05-30 16:17:53 +0200

    bug!

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-var app = new Vue({
+const app = new Vue({
     el: '#app',
     data: {
-
-    }
+        email: "",
+    },
+    created() {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((res) => {
+                console.log(res);
+            }),
+    },
 })
